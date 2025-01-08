9b4b5e8b745e7140f34c3c2fe1085e6cd6a886b8
rithkott <rithkott@gmail.com>
2025-01-08 00:55:24 -0600

    Tried using firebase, api key not working for some reason and I tried everything to debug, will be switching away from firebase most likely. On the upside I made a decent login page with shadcn to test which I will likely keep

--- smip/src/components/Header.tsx
+++ smip/src/components/Header.tsx
@@ -5,7 +5,7 @@ import { ModeToggle } from '@/components/ModeToggle';
 
 export function Header(){
     return (
-        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 z-20">
+        <header className="bg-background h-12 p-2 border-b sticky top-0 z-20">
             <div className="flex h-8 items-center justify-between w-full">
                 <div className="flex items-center gap-2">
                     <NavButton href="/home" label="My Home" icon={HomeIcon} />
@@ -20,7 +20,7 @@ export function Header(){
                     <NavButton href="/mytrades" label="MyTrades" icon={ChartLineIcon} />
                     <NavButton href="/social" label="Social" icon={UsersRound} />
                     <NavButton href="/calculator" label="Calculator" icon={CalculatorIcon} />
-                    <ModeToggle />
+                    <ModeToggle/>
                 </div>
 
             </div>
