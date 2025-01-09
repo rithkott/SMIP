746721220ea0a0a45da3b5af0db7629d095e5913
rithkott <rithkott@gmail.com>
2025-01-09 00:00:25 -0600

    Minor cosmetic changes, most important thing is now everything looks pretty nice and also it fuckin works

--- smip/src/app/(rs)/social/page.tsx
+++ smip/src/app/(rs)/social/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-    title: "Page not found"
+    title: "Social"
 }
 
 export default function Social(){
--- smip/src/app/page.tsx
+++ smip/src/app/page.tsx
@@ -10,27 +10,27 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <BackgroundBeams />
-      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
 
+      <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
+        <BackgroundBeams className="z-[-1]"/>
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
           <h1>Welcome to SMIP</h1> 
           <h1>A stock market inspired game where you can invest in your favorite social media posts</h1>
-          <div className="w-1/4 mx-auto">
+          <Button className="mx-auto justify-center items-center mt-2 mb-4 font-bold">
+            <Link href="/home"> Enter SMIP </Link>
+          </Button>
+          <div className="w-1/6 mx-auto">
             <Link href="https://github.com/rithkott/SMIP">
               <Image 
                 src="/images/github-mark-white.svg" 
                 alt="Link to Github"
                 layout="responsive" 
                 className="rounded-lg mx-auto"
-                width={100}
-                height={100}
+                width={50}
+                height={50}
                 />
             </Link>
           </div>
-          <Button className="mx-auto justify-center items-center mt-10">
-            <Link href="/login"> Enter SMIP </Link>
-          </Button>
         </div>
 
       </main>
